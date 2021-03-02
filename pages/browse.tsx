403e954fd99f86f58607338f9a1527bf0a5f8375
Gabriel Perinazzo <git@gabrielperinazzo.live>
2021-03-02 18:29:14 -0300

    feat: navbar on all pages

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -1,54 +1,101 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import Navbar from '../components/Navbar'
 
 export default function Browse () {
   return (
-    <>
-      <h1>Navegar</h1>
-      <aside className="fixed bottom-0 left-0 z-40 w-full">
-        <Navbar
-          navLinks={[
-            {
-              icon: 'house',
-              title: 'Início',
-              url: '/'
-            },
-            {
-              icon: 'bookmark',
-              title: 'Minha Lista',
-              url: '/my-list'
-            },
-            {
-              icon: 'cobrowsing',
-              title: 'Navegar',
-              url: '/browse',
-              subLinks: [
+    <div className="3xl:container mx-auto pb-16 lg:pb-0 lg:flex">
+      <aside className="fixed bottom-0 left-0 z-40 w-full lg:w-66 lg:relative lg:border-r lg:border-grey-300">
+          <div className="lg:sticky lg:top-0 lg:left-0 lg:overflow-x-hidden">
+            <Link href="/">
+              <a className="hidden lg:flex justify-center shadow-menu">
+                <Image
+                  src="/images/nextlogo.png"
+                  width="244"
+                  height="62"
+                  layout="fixed"
+                />
+              </a>
+            </Link>
+            <Navbar
+              navLinks={[
                 {
-                  title: 'Treinamentos',
-                  url: '/browse'
+                  icon: 'house',
+                  title: 'Início',
+                  url: '/'
                 },
                 {
-                  title: 'Trilhas',
-                  url: '/browse'
+                  icon: 'bookmark',
+                  title: 'Minha Lista',
+                  url: '/my-list'
                 },
                 {
-                  title: 'Podcasts',
-                  url: '/browse'
+                  icon: 'cobrowsing',
+                  title: 'Navegar',
+                  url: '/browse',
+                  subLinks: [
+                    {
+                      title: 'Treinamentos',
+                      url: '/browse'
+                    },
+                    {
+                      title: 'Trilhas',
+                      url: '/browse'
+                    },
+                    {
+                      title: 'Podcasts',
+                      url: '/browse'
+                    }
+                  ]
+                },
+                {
+                  icon: 'squares',
+                  title: 'Categorias',
+                  url: '/categories',
+                  subLinks: [
+                    {
+                      title: 'Ferramentas e Sistemas',
+                      url: '/categories'
+                    },
+                    {
+                      title: 'Gestão Estratégica',
+                      url: '/categories'
+                    },
+                    {
+                      title: 'Produtos',
+                      url: '/categories'
+                    },
+                    {
+                      title: 'testonly',
+                      url: '/categories'
+                    }
+                  ]
+                },
+                {
+                  icon: 'materials',
+                  title: 'Materiais',
+                  alternativeTitle: 'Material de Apoio',
+                  url: '/materials',
+                  subLinks: [
+                    {
+                      title: 'Ferramentas',
+                      url: '/materials'
+                    },
+                    {
+                      title: 'Biblioteca',
+                      url: '/materials'
+                    },
+                    {
+                      title: 'Anotações',
+                      url: '/materials'
+                    }
+                  ]
                 }
-              ]
-            },
-            {
-              icon: 'squares',
-              title: 'Categorias',
-              url: '/categories'
-            },
-            {
-              icon: 'materials',
-              title: 'Materiais',
-              url: '/materials'
-            }
-          ]}
-        />
-      </aside>
-    </>
+              ]}
+            />
+          </div>
+        </aside>
+      <h1>Navegar</h1>
+    </div>
   )
 }
--- pages/post.tsx
+++ pages/post.tsx
@@ -1,40 +1,101 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import Navbar from '../components/Navbar'
 
 export default function Post () {
   return (
-    <>
-      <h1>Pagina post</h1>
-      <aside className="fixed bottom-0 left-0 z-40 w-full">
-      <Navbar
-        navLinks={[
-          {
-            icon: 'house',
-            title: 'Início',
-            url: '/'
-          },
-          {
-            icon: 'bookmark',
-            title: 'Minha Lista',
-            url: '/my-list'
-          },
-          {
-            icon: 'cobrowsing',
-            title: 'Navegar',
-            url: '/browse'
-          },
-          {
-            icon: 'squares',
-            title: 'Categorias',
-            url: '/categories'
-          },
-          {
-            icon: 'materials',
-            title: 'Materiais',
-            url: '/materials'
-          }
-        ]}
-      />
-    </aside>
-    </>
+    <div className="3xl:container mx-auto pb-16 lg:pb-0 lg:flex">
+      <aside className="fixed bottom-0 left-0 z-40 w-full lg:w-66 lg:relative lg:border-r lg:border-grey-300">
+          <div className="lg:sticky lg:top-0 lg:left-0 lg:overflow-x-hidden">
+            <Link href="/">
+              <a className="hidden lg:flex justify-center shadow-menu">
+                <Image
+                  src="/images/nextlogo.png"
+                  width="244"
+                  height="62"
+                  layout="fixed"
+                />
+              </a>
+            </Link>
+            <Navbar
+              navLinks={[
+                {
+                  icon: 'house',
+                  title: 'Início',
+                  url: '/'
+                },
+                {
+                  icon: 'bookmark',
+                  title: 'Minha Lista',
+                  url: '/my-list'
+                },
+                {
+                  icon: 'cobrowsing',
+                  title: 'Navegar',
+                  url: '/browse',
+                  subLinks: [
+                    {
+                      title: 'Treinamentos',
+                      url: '/browse'
+                    },
+                    {
+                      title: 'Trilhas',
+                      url: '/browse'
+                    },
+                    {
+                      title: 'Podcasts',
+                      url: '/browse'
+                    }
+                  ]
+                },
+                {
+                  icon: 'squares',
+                  title: 'Categorias',
+                  url: '/categories',
+                  subLinks: [
+                    {
+                      title: 'Ferramentas e Sistemas',
+                      url: '/categories'
+                    },
+                    {
+                      title: 'Gestão Estratégica',
+                      url: '/categories'
+                    },
+                    {
+                      title: 'Produtos',
+                      url: '/categories'
+                    },
+                    {
+                      title: 'testonly',
+                      url: '/categories'
+                    }
+                  ]
+                },
+                {
+                  icon: 'materials',
+                  title: 'Materiais',
+                  alternativeTitle: 'Material de Apoio',
+                  url: '/materials',
+                  subLinks: [
+                    {
+                      title: 'Ferramentas',
+                      url: '/materials'
+                    },
+                    {
+                      title: 'Biblioteca',
+                      url: '/materials'
+                    },
+                    {
+                      title: 'Anotações',
+                      url: '/materials'
+                    }
+                  ]
+                }
+              ]}
+            />
+          </div>
+        </aside>
+      <h1>Post</h1>
+    </div>
   )
 }
